0727f57f40c4d3c3be7721f5896a4360a6bce382
MA XinXin <xxmawhu@gmail.com>
2026-04-16 17:11:40 +0000

    docs: SEC-103 mount_cleaner.hpp path matching precision documentation

---
+++ build-test2/_deps/toml11-subbuild/toml11-populate-prefix/tmp/toml11-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/mnt/beegfs_data/usr/maxx_ai/ai/ai-mirror/build-test2/_deps/toml11-src"
+  "/mnt/beegfs_data/usr/maxx_ai/ai/ai-mirror/build-test2/_deps/toml11-build"
+  "/mnt/beegfs_data/usr/maxx/ai/ai-mirror/build-test2/_deps/toml11-subbuild/toml11-populate-prefix"
+  "/mnt/beegfs_data/usr/maxx/ai/ai-mirror/build-test2/_deps/toml11-subbuild/toml11-populate-prefix/tmp"
+  "/mnt/beegfs_data/usr/maxx/ai/ai-mirror/build-test2/_deps/toml11-subbuild/toml11-populate-prefix/src/toml11-populate-stamp"
+  "/mnt/beegfs_data/usr/maxx/ai/ai-mirror/build-test2/_deps/toml11-subbuild/toml11-populate-prefix/src"
+  "/mnt/beegfs_data/usr/maxx/ai/ai-mirror/build-test2/_deps/toml11-subbuild/toml11-populate-prefix/src/toml11-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/mnt/beegfs_data/usr/maxx/ai/ai-mirror/build-test2/_deps/toml11-subbuild/toml11-populate-prefix/src/toml11-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/mnt/beegfs_data/usr/maxx/ai/ai-mirror/build-test2/_deps/toml11-subbuild/toml11-populate-prefix/src/toml11-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()

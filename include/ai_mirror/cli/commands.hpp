c459ba1660ad40465c5819c632b63176fa0b1170
MA XinXin <xxmawhu@gmail.com>
2026-05-18 07:30:41 +0000

    feat: add am auto-fix-all command

--- include/ai_mirror/cli/commands.hpp
+++ include/ai_mirror/cli/commands.hpp
@@ -15,20 +15,24 @@
 
 namespace ai_mirror::cli {
 
-int cmd_create(const std::string& project_path, bool verbose);
-int cmd_mkdir(const std::string& path, const std::string& ai_user, bool verbose);
-int cmd_cp(const std::string& src, const std::string& dst, bool verbose);
-int cmd_mv(const std::string& src, const std::string& dst, bool verbose);
-int cmd_touch(const std::string& path, const std::string& ai_user, bool verbose);
-int cmd_cd(const std::string& path, bool verbose);
+int cmd_create(const std::string &project_path, bool verbose);
+int cmd_mkdir(const std::string &path, const std::string &ai_user,
+              bool verbose);
+int cmd_cp(const std::string &src, const std::string &dst, bool verbose);
+int cmd_mv(const std::string &src, const std::string &dst, bool verbose);
+int cmd_touch(const std::string &path, const std::string &ai_user,
+              bool verbose);
+int cmd_cd(const std::string &path, bool verbose);
 int cmd_list(bool verbose);
 int cmd_health(bool verbose);
-int cmd_force_destroy(const std::string& project_or_user, bool verbose);
-int cmd_rm(const std::string& project_path, bool verbose);
+int cmd_force_destroy(const std::string &project_or_user, bool verbose);
+int cmd_rm(const std::string &project_path, bool verbose);
 int cmd_config(bool verbose);
 int cmd_status(bool verbose);
-int cmd_update(const std::string& project_path, bool verbose);
-int cmd_watch(const std::string& watch_path, const std::string& watch_user, bool verbose);
+int cmd_update(const std::string &project_path, bool verbose);
+int cmd_watch(const std::string &watch_path, const std::string &watch_user,
+              bool verbose);
 int cmd_init(bool verbose);
+int cmd_auto_fix_all(bool verbose);
 
-}
+} // namespace ai_mirror::cli

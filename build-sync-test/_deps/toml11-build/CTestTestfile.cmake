879a88a5acf6dbed4e8d5f6507b75d98592d3a59
MA XinXin <xxmawhu@gmail.com>
2026-06-05 01:25:31 +0000

    chore: remove build-sync-test directory

--- build-sync-test/_deps/toml11-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/beegfs_data/usr/maxx/dev/aimirror/ai-mirror/github-ai-mirror/build-sync-test/_deps/toml11-src
-# Build directory: /mnt/beegfs_data/usr/maxx/dev/aimirror/ai-mirror/github-ai-mirror/build-sync-test/_deps/toml11-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
